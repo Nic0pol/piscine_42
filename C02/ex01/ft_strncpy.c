81e3f395daf88b989df3c3fb2837cf582e431ba3
Nic0pol <140616790+Nic0pol@users.noreply.github.com>
2023-12-12 17:40:30 +0100

    Add files via upload

---
+++ C02/ex01/ft_strncpy.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: locastel <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/07 18:21:02 by locastel          #+#    #+#             */
+/*   Updated: 2023/12/12 17:24:57 by locastel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_strncpy(char *dest, char *src, unsigned int n)
+{
+	char	*original_dest;
+
+	original_dest = dest;
+	while (n > 0 && *src)
+	{
+		*dest++ = *src++;
+		n--;
+	}
+	while (n > 0)
+	{
+		*dest++ = '\0';
+		n--;
+	}
+	return (original_dest);
+}
